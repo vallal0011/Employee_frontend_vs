5938e27e7c764d0b8732aaa993d9015644ed47a0
vallal0011@gmail.com <vallal0011@gmail.com>
2023-11-01 17:32:30 +0530

    Updated_variable

--- src/Variable.js
+++ src/Variable.js
@@ -1,4 +1,7 @@
 export const variables={
-    API_URL:"http://localhost:5193/api/",
-    PHOTO_URL:"http://localhost:5193/Photos/"
+    // API_URL:"http://localhost:5193/api/",
+    // PHOTO_URL:"http://localhost:5193/Photos/"
+
+    API_URL:"https://emplyui.azurewebsites.net/api/",
+    PHOTO_URL:"https://emplyui.azurewebsites.net/Photos/"
 }
