ae05a3d18e8b110ff22f1e6500abe3b76defabaf
vallal0011@gmail.com <vallal0011@gmail.com>
2023-11-06 15:51:27 +0530

    updated _test_07

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Intro from './Intro';
 
 test('renders learn react link', () => {
-  render(<Intro />);
-  const linkElement = screen.getByText(/Login/i);
+  render(
+     <BrowserRouter>
+       <Intro />
+     </BrowserRouter>
+  );
+ 
+  const linkElement = screen.getByText(/This is Home page/i);
   expect(linkElement).toBeInTheDocument();
-});
+ });
