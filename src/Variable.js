b0dc25b45ef713243bd35088987bcc4426bcd0a4
vallal0011@gmail.com <vallal0011@gmail.com>
2023-11-06 14:28:55 +0530

    updated backend url

--- src/Variable.js
+++ src/Variable.js
@@ -2,6 +2,6 @@ export const variables={
     // API_URL:"http://localhost:5193/api/",
     // PHOTO_URL:"http://localhost:5193/Photos/"
 
-    API_URL:"https://emplyui.azurewebsites.net/api/",
-    PHOTO_URL:"https://emplyui.azurewebsites.net/Photos/"
+    API_URL:"https://empyapi.azurewebsites.net/api/",
+    PHOTO_URL:"https://empyapi.azurewebsites.net/Photos/"
 }
